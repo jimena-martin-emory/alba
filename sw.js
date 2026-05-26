0afb087daf6e6ff31ee20846186e963b954f10e7
jimena-martin-emory <111389828+jimena-martin-emory@users.noreply.github.com>
2026-05-26 16:58:25 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'alba-v1';
+const ASSETS = ['/', '/index.html', 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
